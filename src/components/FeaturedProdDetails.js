a6bd70f97fd7cff2664b32d0e4ed86c27b562ff2
Sararid <sarawen88@gmail.com>
2021-11-17 18:48:58 +0100

    add info products in shop component and styling of single prod componenet

--- src/components/FeaturedProdDetails.js
+++ src/components/FeaturedProdDetails.js
@@ -1,52 +1,61 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useParams } from 'react-router';
-import { useDispatch, useSelector } from 'react-redux';
-import axios from 'axios';
-import { selectedProducts, removeSelectedProducts } from '../redux/actions/productActions';
+import React from "react";
+import { useEffect } from "react";
+import { useParams } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import {
+    selectedProducts,
+    removeSelectedProducts,
+} from "../redux/actions/productActions";
+import { Card, Button } from "react-bootstrap";
 
 const ProductDetails = () => {
-
     const products = useSelector((state) => state.products);
     const { id, title, price, category, image, description } = products;
-    const { productId } = useParams()
-    const dispatch = useDispatch()
+    const { productId } = useParams();
+    const dispatch = useDispatch();
 
-    const fetchProductDetails =
-        async () => {
-            const response = await axios
-                .get(`https://fakestoreapi.com/products/${productId}`)
-                .catch((err) => { console.log("err", err) })
-            dispatch(selectedProducts(response.data))
-        }
+    const fetchProductDetails = async () => {
+        const response = await axios
+            .get(`https://fakestoreapi.com/products/${productId}`)
+            .catch((err) => {
+                console.log("err", err);
+            });
+        dispatch(selectedProducts(response.data));
+    };
 
     useEffect(() => {
         if (productId && productId !== "") fetchProductDetails();
         return () => {
-            dispatch(removeSelectedProducts())
-        }
+            dispatch(removeSelectedProducts());
+        };
     }, [productId]);
 
-
     return (
-        <div>
-            {Object.keys(products).length === 0 ? (<div>
-                ...Loading </div>) : (
-                <ul>
-                    <li id={id}>
-                        <h2> {title} </h2>
-                        <img src={image} alt={title} />
-                        <span> {price}</span>
-                        <span> {category}</span>
-                        <p> {description}</p>
+        <div className='singleProduct__container'>
+            {Object.keys(products).length === 0 ? (
+                <div>...Loading </div>
+            ) : (
+                <li key={id} id={id} className="singleProduct">
+                    <Card style={{ width: "25rem" }}>
+                        <Card.Img variant="top" src={image} alt={title} />
+                        <Card.Body>
+                            <Card.Title>{title}</Card.Title>
 
-                    </li>
-                </ul>
+                            <Card.Subtitle className="mb-2 text-muted">
+                                {category}
+                            </Card.Subtitle>
+                            <span> € {price}</span>
 
+                            <Card.Text>{description} </Card.Text>
+
+                            <Button>Add to cart</Button>
+                        </Card.Body>
+                    </Card>
+                </li>
             )}
-
         </div>
-    )
-}
+    );
+};
 
 export default ProductDetails;
--- src/components/pages/Shop.js
+++ src/components/pages/Shop.js
@@ -1,11 +1,59 @@
-import React from 'react'
+import React from 'react';
+import FeaturedProduct from '../FeaturedProduct';
+import '../../styles/Layout/Featured.scss';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { setProducts } from './../../redux/actions/productActions'
+import { useEffect } from 'react';
 
 const Shop = () => {
+
+
+    const products = useSelector((state) => state)
+    const dispatch = useDispatch();
+
+    const fetchProducts =
+        () => {
+            // Llamamos al API
+            return fetch('https://fakestoreapi.com/products') //
+                .then(response => response.json())
+
+                .then(response => {
+                    // Cuando responde el API podemos limpiar los datos aquí
+                    const result = response
+
+                        .map(item => {
+                            return {
+                                id: item.id,
+                                title: item.title,
+                                description: item.description,
+                                price: item.price,
+                                image: item.image,
+                                category: item.category
+                            }
+
+                        });
+                    dispatch(setProducts(result));
+                })
+                .catch(error => { console.log('err', error) });
+        };
+
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    console.log('products are :' + products)
+
     return (
-        <div>
-            lista de todos los articulos
-        </div>
+        < >
+            <section>
+                <h2 className="featured__header">Shop now</h2>
+                <div className="featured__product">
+                    <FeaturedProduct />
+                </div></section>
+        </>
     )
 }
 
-export default Shop
+export default Shop;
